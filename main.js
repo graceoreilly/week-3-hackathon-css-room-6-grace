86a31e3487dd2c0f68578f8de0bf1b09a0f2322a
Conner Adamsons <185359332+ConnerConner96@users.noreply.github.com>
2024-12-12 14:12:07 +0000

    ISSUE #2, #3 and #5 complete

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 console.log("hello")
+
+async function CatGenerator() {
+const response = await fetch ("https://cataas.com/cat", {
+    headers: {
+    Accept: "application/blob"}
+});
+;
+console.log(response);
+const blob = await response.blob();
+console.log();
+
+const imageURL = URL.createObjectURL(blob);
+
+const img = document.createElement("img");
+img.src = imageURL;
+img.alt = "Random Cat";
+document.body.appendChild(img);
+
+return blob;
+}
+CatGenerator();
+
+// function fetchImage() {
+// fetch ("https://cataas.com/cat", {
+//       method: 'GET',
+//       headers: {
+//         Accept: "application/json"
+//       }
+//     })
+//       .then(response => response.blob())
+//       .then(data => {
+//         // Do something with the image data
+//             console.log(data)
+//       })
+//       .catch(error => console.error(error));
+//   }
