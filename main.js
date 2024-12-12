0370cb4e24704756c2e525e0ef843a5b0b7954ca
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-12 15:03:42 +0000

    Issue #4
    Added try and catch statements to test for errors

--- main.js
+++ main.js
@@ -1,37 +1,27 @@
-console.log("hello")
-
 async function CatGenerator() {
+    try { //this is the block of code to be tested for errors while it is being executed
 const response = await fetch ("https://cataas.com/cat", {
     headers: {
     Accept: "application/blob"}
 });
-;
+if (!response.ok) {
+            throw new Error(`THERE IS AN ERROR! status: ${response.status}`);
+    }
 console.log(response);
-const blob = await response.blob();
-console.log();
+const blob = await response.blob(); //converts the response to a blob (binary image data), await is used as this is an async operation //converts it to a blob object
 
-const imageURL = URL.createObjectURL(blob);
+const imageURL = URL.createObjectURL(blob); //created a URL which points to the blob object, allowing it to be displayed as an image in the browser
 
-const img = document.createElement("img");
-img.src = imageURL;
-img.alt = "Random Cat";
-document.body.appendChild(img);
+const img = document.createElement("img"); //creates a new img element
+img.src = imageURL; //sets the image src attribute of img element to the imageURL
+img.alt = "Random Cat"; //sets the alt attribute of the image
+document.body.appendChild(img); //appends the image to the body, you have to append an element in order to add the element to the DOM
 
 return blob;
 }
-CatGenerator();
+catch (error) { //this is the block of code to be executed, if an error occurs in the try block
+     console.error('Error fetching data:', error);
+    }
+}
 
-// function fetchImage() {
-// fetch ("https://cataas.com/cat", {
-//       method: 'GET',
-//       headers: {
-//         Accept: "application/json"
-//       }
-//     })
-//       .then(response => response.blob())
-//       .then(data => {
-//         // Do something with the image data
-//             console.log(data)
-//       })
-//       .catch(error => console.error(error));
-//   }
+CatGenerator();
